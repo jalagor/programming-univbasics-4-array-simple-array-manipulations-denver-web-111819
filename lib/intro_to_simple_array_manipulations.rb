ca62b0d44575a8e7dc51dd6539e5952d0b8e3f99
John Rogala <johnrossrogala@gmail.com>
2019-11-11 00:24:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,40 @@
+
+
+def using_push(array, element)
+  but = ["what", "do", "you", "want"]
+  new = "violet" 
+  but.push(new)
+end  
+
+def using_unshift(array, element) 
+  array.unshift(element)
+  
+  
+end 
+  
+def using_pop(array)
+  array.pop
+  #but = ["what", "do", "five", "four", "six", "no", "Antarctica"] 
+  #but.pop
+  #answr = but.size
+  #puts answr 
+  #puts "Antarctica"
+  #puts but
+end 
+ 
+def pop_with_args(array) 
+  array.pop(2)
+end 
+ 
+def using_shift(array)
+  array.shift
+end 
+
+def shift_with_args(array)
+  array.shift(2)
+end 
+
+def using_concat(array)
+  array.concat 
+end
+  
